382106eec302f5f9e7aca4d96de9c4af43c78cbb
Bunyodbek G'ulomjonov <bunyodbek@Bunyodbeks-MacBook-Air.local>
2026-01-25 16:47:39 +0500

    login dto ficed

--- src/modules/users/dto/login-user.dto.ts
+++ src/modules/users/dto/login-user.dto.ts
@@ -3,7 +3,6 @@ import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class LoginUserDto{
     @ApiProperty({ example: 'username12', description: 'userName(Masalan bun6yoodbek123)' })
-      @IsString()
       userName: string;
     
       @ApiProperty({ example: 'password123', description: 'Maxfiy parol' })
